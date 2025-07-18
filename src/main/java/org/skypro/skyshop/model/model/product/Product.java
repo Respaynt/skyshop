a7a8a8e8be69daee90a0b0135dd0542610f3e949
Первый комит <suzdalcev97t@gmail.com>
2025-07-18 22:44:38 +0400

    gotovo

---
+++ src/main/java/org/skypro/skyshop/model/model/product/Product.java
@@ -0,0 +1,65 @@
+package org.skypro.skyshop.model.model.product;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.skypro.skyshop.model.model.search.Searchable;
+
+import java.util.Objects;
+import java.util.UUID;
+public abstract class Product implements Searchable {
+    private final UUID id;
+    private final String name;
+
+    public Product(UUID id, String name) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID не может быть null");
+        }
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Название продукта не может быть пустой строкой или null");
+        }
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public abstract int getPrice();
+
+    public boolean isSpecial() {
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);  // сравниваем по id
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    @JsonIgnore
+    public String getSearchTerm() {
+        return name;
+    }
+
+    @Override
+    @JsonIgnore
+    public String getContentType() {
+        return "PRODUCT";
+    }
+
+    @Override
+    public abstract String toString();
+}
---
+++ src/main/java/org/skypro/skyshop/model/model/search/SearchResult.java
@@ -0,0 +1,57 @@
+package org.skypro.skyshop.model.model.search;
+
+import java.util.Objects;
+
+public final class SearchResult {
+    private final String id;
+    private final String name;
+    private final String contentType;
+
+    public SearchResult(String id, String name, String contentType) {
+        this.id = id;
+        this.name = name;
+        this.contentType = contentType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public static SearchResult fromSearchable(Searchable item) {
+        return new SearchResult(
+                item.getId().toString(),
+                item.getName(),
+                item.getContentType()
+        );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SearchResult)) return false;
+        SearchResult that = (SearchResult) o;
+        return id.equals(that.id) && name.equals(that.name) && contentType.equals(that.contentType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, contentType);
+    }
+
+    @Override
+    public String toString() {
+        return "SearchResult{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", contentType='" + contentType + '\'' +
+                '}';
+    }
+}
