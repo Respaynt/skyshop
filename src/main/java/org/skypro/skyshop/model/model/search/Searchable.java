c138af628a2a9b0491cbcada381ca423b27e024c
Первый комит <suzdalcev97t@gmail.com>
2025-07-19 14:11:46 +0400

    Готово

---
+++ src/main/java/org/skypro/skyshop/model/model/search/Searchable.java
@@ -0,0 +1,19 @@
+package org.skypro.skyshop.model.model.search;
+
+import java.util.UUID;
+
+public interface Searchable {
+    UUID getId();
+
+    String getSearchTerm();
+
+    String getContentType();
+
+    String getName();
+
+    default String getStringRepresentation() {
+        return getName() + " — " + getContentType();
+    }
+}
+
+
